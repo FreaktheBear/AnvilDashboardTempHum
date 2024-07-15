5431db1f122bdd8c0be0b7c4794866b302a81da6
freekdenbeer@gmail.com <freekdenbeer@gmail.com>
2024-07-15 00:34:48 +0000

    Edited Humidity, settings, Temperature and Frame

--- client_code/Frame/__init__.py
+++ client_code/Frame/__init__.py
@@ -5,6 +5,8 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Humidity import Humidity
+from ..Temperature import Temperature
 
 #This is your startup form. It has a sidebar with navigation links and a content panel where page content will be added.
 class Frame(FrameTemplate):
@@ -17,7 +19,7 @@ class Frame(FrameTemplate):
     #Set the Plotly plots template to match the theme of the app
     Plot.templates.default = "rally"
     #When the app starts up, the Temperature form will be added to the page
-    self.content_panel.add_component(Sales())
+    self.content_panel.add_component(Temperature())
     #Change the color of the temp_page_link to indicate that the Sales page has been selected
     self.sales_page_link.background = app.theme_colors['Primary Container']
     
@@ -26,7 +28,7 @@ class Frame(FrameTemplate):
     """This method is called when the link is clicked"""
     #Clear the content panel and add the Temperature Form
     self.content_panel.clear()
-    self.content_panel.add_component(Sales())
+    self.content_panel.add_component(Temperature())
     #Change the color of the temp_page_link to indicate that the Temperature page has been selected
     self.sales_page_link.background = app.theme_colors['Primary Container']
     self.reports_page_link.background = "transparent"
@@ -35,7 +37,7 @@ class Frame(FrameTemplate):
     """This method is called when the link is clicked"""
     #Clear the content panel and add the Humidity Form
     self.content_panel.clear()
-    self.content_panel.add_component(Reports())
+    self.content_panel.add_component(Humidity())
     #Change the color of the hum_page_link to indicate that the Humidity page has been selected
     self.reports_page_link.background = app.theme_colors['Primary Container']
     self.sales_page_link.background = "transparent"
